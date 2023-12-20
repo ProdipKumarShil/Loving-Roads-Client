353bbb5314c7afa36efaa36782b99b488559d66b
ProdipKumarShil <prodipkrishna01@gmail.com>
2023-12-20 16:45:02 +0600

    top destainations completed

--- src/components/TopDestinations/TopDestinations.jsx
+++ src/components/TopDestinations/TopDestinations.jsx
@@ -1,16 +1,27 @@
 import bg from '../../assets/TopDestinations/p.jpg'
+import food from '../../assets/categories/food.jpg'
+import game from '../../assets/categories/game.jpg'
+import travel from '../../assets/categories/travel.jpg'
+import programming from '../../assets/categories/programming.jpg'
+import movie from '../../assets/categories/movie.jpg'
+
+const categories = [
+  {text: "Travel", img: travel},
+  {text: "Game", img: game},
+  {text: "Programming", img: programming},
+  {text: "Movie", img: movie},
+  {text: "Food", img: food},
+]
 
 const TopDestinations = () => {
   return (
     <div className='max-w-7xl mx-auto mb-40'>
-      <p className='text-2xl font-bold mb-2'>Top Destinations</p>
-      <p className='font-bold mb-6'>Tick one more destination off of your bucket list with one of our most popular vacations in 2022</p>
-      <div className="grid md:grid-cols-3 lg:grid-cols-5 gap-2">
-        <PhotoContainers />
-        <PhotoContainers />
-        <PhotoContainers />
-        <PhotoContainers />
-        <PhotoContainers />
+      <p className='text-2xl font-bold mb-2 px-3'>Top Destinations</p>
+      <p className='font-bold mb-6 px-3'>Tick one more destination off of your bucket list with one of our most popular vacations in 2022</p>
+      <div className="grid md:grid-cols-3 lg:grid-cols-5 gap-4 px-4">
+        {
+          categories.map((category, index) => <PhotoContainers key={index} category={category} />)
+        }
       </div>
     </div>
   );
@@ -18,11 +29,11 @@ const TopDestinations = () => {
 
 export default TopDestinations;
 
-const PhotoContainers = () => {
+const PhotoContainers = ({category}) => {
   return(
-    <div className="relative">
-      <img className='h-[230px] object-cover brightness-90' src={bg} alt="" />
-      <p className='text-xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center font-bold text-slate-100'>Lorem, ipsum dolor.</p>
+    <div className="relative group overflow-hidden hover:scale-105 md:hover:scale-110 duration-500">
+      <img className='h-[230px] w-full object-cover group-hover:brightness-50 cursor-pointer duration-500' src={category.img} alt="" />
+      <p className='text-xl w-full absolute top-60 left-72 opacity-0 duration-500 group-hover:opacity-100 group-hover:top-1/2 group-hover:left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center font-bold text-slate-100'>{category.text}</p>
     </div>
   )
 }
