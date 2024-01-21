d216e16971c9635ac4a87d44f06a0a69f0564af7
ProdipKumarShil <prodipkrishna01@gmail.com>
2024-01-21 11:12:16 +0600

    fix user img and date

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,27 +1,27 @@
-import img from '../../assets/icons/img.svg'
+import moment from 'moment';
+import img2 from '../../assets/icons/img.svg'
 
-const Card = () => {
+const Card = ({blog}) => {
+  const {title, body, category, date, img, author} = blog
   return (
-    <div className="border group">
+    <div className="border group h-full">
       <figure className="mb-6 relative">
         <img
-          className="w-full"
-          src="https://images.unsplash.com/photo-1606419704159-ace37c2b177b?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          className="w-full h-[300px] object-cover"
+          src={img.img}
           alt=""
         />
         <div className="absolute inset-2 text-white hidden group-hover:block">
-          <span className="mr-3 py-1 px-2 bg-slate-400 bg-opacity-70 rounded-md font-semibold text-sm">dddd</span>
-          <span className=" py-1 px-2 bg-slate-400 bg-opacity-70 rounded-md font-semibold text-sm">dddd</span>
+          <span className="mr-3 py-1 px-2 bg-slate-400 bg-opacity-70 rounded-md font-semibold text-sm">{category.toUpperCase()}</span>
+          {/* <span className=" py-1 px-2 bg-slate-400 bg-opacity-70 rounded-md font-semibold text-sm">dddd</span> */}
         </div>
-        <img className='absolute top-2 right-2 bg-slate-400 p-1 bg-opacity-70 rounded-full hidden group-hover:block' src={img} alt="" />
+        <img className='absolute top-2 right-2 bg-slate-400 p-1 bg-opacity-70 rounded-full hidden group-hover:block' src={img2} alt="" />
       </figure>
       <div className="px-4">
-        <p className="text-2xl font-bold mb-5">Integer Maecenas Eget Viverra</p>
-        <User />
+        <p className="text-2xl font-bold mb-5 hover:underline">{title}</p>
+        <User user={author} date={date} />
         <p className="text-[#6C757D] text-sm mb-4">
-          Aenean eleifend ante maecenas pulvinar montes lorem et pede dis dolor
-          pretium donec dictum. Vici consequat justo enim. Venenatis eget
-          adipiscing luctus lorem.
+          {body[0].substring(0, 200)}...
         </p>
         <button className="font-bold text-[#121416] underline mb-3">View More</button>
       </div>
@@ -31,14 +31,14 @@ const Card = () => {
 
 export default Card;
 
-const User = () => {
-  return (
-    <div className="flex items-center gap-4 mb-4">
-      <div className="w-10 h-10 rounded-full bg-slate-500"></div>
+const User = ({user, date}) => {
+  const letterOfName = user.name.toUpperCase().split('')[0]
+  return (<div className="flex items-center gap-4 mb-4">
+      {user.img ? <img className='w-10 h-10 rounded-full' src={user.img} /> : <div className="w-10 h-10 rounded-full bg-slate-500 flex justify-center items-center"><span className='block font-bold text-white '>{letterOfName}</span></div>}
       <div className="flex items-center md:gap-4 flex-col md:flex-row md:flex-grow">
-        <p className="text-[#121416] text- font-bold">John Weak</p>
+        <p className="text-[#121416] text- font-bold">{user.name}</p>
         <hr className="flex-grow  hidden md:block" />
-        <p className="text-[#6C757D] text-sm">June 28, 2018</p>
+        <p className="text-[#6C757D] text-sm">{moment(date).format('D MMM, YYYY')}</p>
       </div>
     </div>
   );
--- src/components/Card/CardContainer.jsx
+++ src/components/Card/CardContainer.jsx
@@ -1,15 +1,19 @@
+import { useEffect, useState } from "react";
 import Button from "../Button";
 import Card from "./Card";
+import axios from "axios";
 
 const CardContainer = () => {
+  const [blogs, setBlogs] = useState([])
+  useEffect(() => {
+    axios.get('https://loving-roads-server.vercel.app/blog/getBlogs')
+      .then(res => setBlogs(res.data.blogs))
+      .catch(e => console.log(e.message))
+  }, [])
   return (
     <div className="max-w-7xl mx-auto mb-40 px-2">
       <div className=" grid md:grid-cols-2 lg:grid-cols-3 gap-2">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {blogs?.map(blog => <Card key={blog._id} blog={blog} />)}
       </div>
       <div className="flex">
         <Button className="mx-auto my-8 border" text="See More"/>
